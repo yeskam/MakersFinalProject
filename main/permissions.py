1384433dc7c7dcf563f35f5acd01836af6996d3b
Yeskam <nursolto01@gmail.com>
2021-04-23 17:25:55 +0600

    first commit

---
+++ main/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsProductAuthor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user == obj.added_by
+
+
+
+class IsCommentAuthor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user == obj.author
